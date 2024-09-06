41988c627d86c3af66da478bc2f4ed3451f02970
MRecioO <matiasrecioojeda@gmail.com>
2024-09-06 01:42:34 -0300

    entrega

--- src/main/java/com/template/Elevator.java
+++ src/main/java/com/template/Elevator.java
@@ -1,11 +1,47 @@
 package com.template;
 
 public class Elevator {
+    int pisoActual;
+    int pisoMax;
+    int pisoMin;
+    int cantPersonas;
+    int capMax;
 
+    public Elevator(int pisoMin, int pisoMax, int capMax) {
+        this.pisoMin = pisoMin;
+        this.pisoMax = pisoMax;
+        this.capMax = capMax;
+    }
 
+    public int getCurrentFloor() {
+        return pisoActual;
+    }
 
-    // Implement this class by observing the tests in this repo. From those you will be able
-    // to pick what the constructors params are, what the methods are and what they should do.
+    public int setCurrentFloor(int nuevoPiso) {
+        if (nuevoPiso <= pisoMax && nuevoPiso >= pisoMin) {
+            this.pisoActual = nuevoPiso;
+            }
+        return pisoActual;
+    }
 
+    public void moveToFloor(int donde) {
+        pisoActual = setCurrentFloor(donde);
+    }
 
+    public int getPassengerCount() {
+        return cantPersonas;
+    }
+
+    public int setPassengerCount(int masPersonas) {
+        if ((getPassengerCount() + masPersonas) <= capMax) {
+            this.cantPersonas += masPersonas;
+        }return cantPersonas;
+    }
+
+    public void addPassenger() {
+        cantPersonas = setPassengerCount(1);
+    }
+    public void removePassenger( ){
+        cantPersonas = setPassengerCount(-1);
+    }
 }
